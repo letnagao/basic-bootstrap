19e665f152c60252f2fa76e131fd2342fb7820b9
letnagao <leticia-nagao@hotmail.com>
2023-07-05 20:54:23 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const earningsChart = document.getElementById('earnings-chart').getContext('2d');
+
+new Chart(earningsChart, {
+  type: 'line',
+  data: {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+    datasets: [
+      {
+        label: 'Earnings',
+        data: [1000, 1500, 1200, 1800, 2000, 1700],
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 2,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true,
+        reverse: false,
+      },
+    },
+  },
+});
